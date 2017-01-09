5372b2f89e0868d5f921478695769880197fe0cf
Leo Lamprecht <mindrun@icloud.com>
2017-01-09 22:07:23 +0100

    Lint with XO

--- index.js
+++ index.js
@@ -13,42 +13,44 @@ module.exports = async (req, res) => {
 cacheData()
 setInterval(cacheData, ms('15m'))
 
-function log (text) {
+function log(text) {
   return slack(text, 'T0CAQ00TU/B3BTWAJLR/Lyo5DjtFTFILfRPfUAa5yLoQ')
 }
 
-function logError (text) {
+function logError(text) {
   return slack(text, 'T0CAQ00TU/B3D9078A3/hlmwh17yhjGztTKmBzCQG7lV')
 }
 
-function slack (text, id) {
+function slack(text, id) {
   fetch(`https://hooks.slack.com/services/${id}`, {
     method: 'POST',
-    body: JSON.stringify({ text })
+    body: JSON.stringify({text})
   })
 }
 
-function cacheData () {
+function cacheData() {
   const start = Date.now()
   fetch('https://api.github.com/repos/zeit/now-cli/releases/latest', {
     headers: {
       Accept: 'application/vnd.github.preview'
     }
   })
-  .then((res) => {
-    if (200 !== res.status) {
+  .then(res => {
+    if (res.status !== 200) {
       return logError('Non-200 response code from GitHub: ' + res.status)
     }
     return res.json()
   })
-  .then((data_) => {
-    if (!data_) return
+  .then(data_ => {
+    if (!data_) {
+      return
+    }
     data = data_
 
     log(`Re-built now releases cache. ` +
-        `Elapsed: ${(new Date - start)}ms`)
+        `Elapsed: ${(new Date() - start)}ms`)
   })
-  .catch((err) => {
+  .catch(err => {
     logError('Error parsing response from GitHub: ' + err.stack)
   })
 }
